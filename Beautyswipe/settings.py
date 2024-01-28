e62e0a6bf63f9a82cdcc84d7320f95d31dda3767
lumiqueV1 <157773031+lumiqueV1@users.noreply.github.com>
2024-01-28 05:33:08 +0530

    Update settings.py

--- Beautyswipe/settings.py
+++ Beautyswipe/settings.py
@@ -38,18 +38,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    "django.middleware.common.CommonMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
- 
 ]
+
 
 ROOT_URLCONF = 'Beautyswipe.urls'
 
