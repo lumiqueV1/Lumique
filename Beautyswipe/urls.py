aca5de5672e2aff0cd3aefd765fdcada8eb4573d
lumiqueV1 <157773031+lumiqueV1@users.noreply.github.com>
2024-01-29 06:02:21 +0530

    Update urls.py

--- Beautyswipe/urls.py
+++ Beautyswipe/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
 ]
 
 # Use += to add static/media URL patterns
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
